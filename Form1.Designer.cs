ed0227351d49a3f18f93f1eeef5ee1d37c012fc3
mahiro <65147170+mahiro0802@users.noreply.github.com>
2022-12-25 03:10:22 +0900

    初回版

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿
+namespace シンコメビュ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.フォント設定ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.最新までスクロールToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userid_box = new System.Windows.Forms.TextBox();
+            this.connect_button = new System.Windows.Forms.Button();
+            this.radio_new = new System.Windows.Forms.RadioButton();
+            this.radio_sub = new System.Windows.Forms.RadioButton();
+            this.radio_kansya = new System.Windows.Forms.RadioButton();
+            this.radio_other = new System.Windows.Forms.RadioButton();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.コピーCToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ユーザーページを開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.コメントページを開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 74);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(580, 505);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            this.dataGridView1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.dataGridView1_Scroll);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "コメント";
+            this.Column1.Name = "Column1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem,
+            this.設定ToolStripMenuItem,
+            this.最新までスクロールToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(604, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.終了ToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(95, 6);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            // 
+            // 設定ToolStripMenuItem
+            // 
+            this.設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.フォント設定ToolStripMenuItem});
+            this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
+            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.設定ToolStripMenuItem.Text = "設定";
+            // 
+            // フォント設定ToolStripMenuItem
+            // 
+            this.フォント設定ToolStripMenuItem.Name = "フォント設定ToolStripMenuItem";
+            this.フォント設定ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+            this.フォント設定ToolStripMenuItem.Text = "フォント設定";
+            this.フォント設定ToolStripMenuItem.Click += new System.EventHandler(this.フォント設定ToolStripMenuItem_Click);
+            // 
+            // 最新までスクロールToolStripMenuItem
+            // 
+            this.最新までスクロールToolStripMenuItem.Name = "最新までスクロールToolStripMenuItem";
+            this.最新までスクロールToolStripMenuItem.Size = new System.Drawing.Size(107, 20);
+            this.最新までスクロールToolStripMenuItem.Text = "最新までスクロール";
+            this.最新までスクロールToolStripMenuItem.Click += new System.EventHandler(this.最新までスクロールToolStripMenuItem_Click);
+            // 
+            // userid_box
+            // 
+            this.userid_box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userid_box.Enabled = false;
+            this.userid_box.Location = new System.Drawing.Point(12, 27);
+            this.userid_box.Name = "userid_box";
+            this.userid_box.Size = new System.Drawing.Size(499, 19);
+            this.userid_box.TabIndex = 2;
+            this.userid_box.Text = "c:gomikuzunokiwami";
+            // 
+            // connect_button
+            // 
+            this.connect_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.connect_button.Location = new System.Drawing.Point(517, 25);
+            this.connect_button.Name = "connect_button";
+            this.connect_button.Size = new System.Drawing.Size(75, 23);
+            this.connect_button.TabIndex = 3;
+            this.connect_button.Text = "接続";
+            this.connect_button.UseVisualStyleBackColor = true;
+            this.connect_button.Click += new System.EventHandler(this.connect_button_Click);
+            // 
+            // radio_new
+            // 
+            this.radio_new.AutoSize = true;
+            this.radio_new.Checked = true;
+            this.radio_new.Location = new System.Drawing.Point(12, 52);
+            this.radio_new.Name = "radio_new";
+            this.radio_new.Size = new System.Drawing.Size(42, 16);
+            this.radio_new.TabIndex = 4;
+            this.radio_new.TabStop = true;
+            this.radio_new.Text = "シン";
+            this.radio_new.UseVisualStyleBackColor = true;
+            this.radio_new.CheckedChanged += new System.EventHandler(this.radio_new_CheckedChanged);
+            // 
+            // radio_sub
+            // 
+            this.radio_sub.AutoSize = true;
+            this.radio_sub.Location = new System.Drawing.Point(60, 52);
+            this.radio_sub.Name = "radio_sub";
+            this.radio_sub.Size = new System.Drawing.Size(42, 16);
+            this.radio_sub.TabIndex = 5;
+            this.radio_sub.TabStop = true;
+            this.radio_sub.Text = "サブ";
+            this.radio_sub.UseVisualStyleBackColor = true;
+            this.radio_sub.CheckedChanged += new System.EventHandler(this.radio_sub_CheckedChanged);
+            // 
+            // radio_kansya
+            // 
+            this.radio_kansya.AutoSize = true;
+            this.radio_kansya.Location = new System.Drawing.Point(108, 52);
+            this.radio_kansya.Name = "radio_kansya";
+            this.radio_kansya.Size = new System.Drawing.Size(47, 16);
+            this.radio_kansya.TabIndex = 6;
+            this.radio_kansya.TabStop = true;
+            this.radio_kansya.Text = "感謝";
+            this.radio_kansya.UseVisualStyleBackColor = true;
+            this.radio_kansya.CheckedChanged += new System.EventHandler(this.radio_kansya_CheckedChanged);
+            // 
+            // radio_other
+            // 
+            this.radio_other.AutoSize = true;
+            this.radio_other.Location = new System.Drawing.Point(161, 52);
+            this.radio_other.Name = "radio_other";
+            this.radio_other.Size = new System.Drawing.Size(54, 16);
+            this.radio_other.TabIndex = 7;
+            this.radio_other.TabStop = true;
+            this.radio_other.Text = "その他";
+            this.radio_other.UseVisualStyleBackColor = true;
+            this.radio_other.CheckedChanged += new System.EventHandler(this.radio_other_CheckedChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.コピーCToolStripMenuItem,
+            this.ユーザーページを開くToolStripMenuItem,
+            this.コメントページを開くToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(167, 70);
+            // 
+            // コピーCToolStripMenuItem
+            // 
+            this.コピーCToolStripMenuItem.Name = "コピーCToolStripMenuItem";
+            this.コピーCToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.コピーCToolStripMenuItem.Text = "コピー(&C)";
+            this.コピーCToolStripMenuItem.Click += new System.EventHandler(this.コピーCToolStripMenuItem_Click);
+            // 
+            // ユーザーページを開くToolStripMenuItem
+            // 
+            this.ユーザーページを開くToolStripMenuItem.Name = "ユーザーページを開くToolStripMenuItem";
+            this.ユーザーページを開くToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.ユーザーページを開くToolStripMenuItem.Text = "ユーザーページを開く";
+            this.ユーザーページを開くToolStripMenuItem.Click += new System.EventHandler(this.ユーザーページを開くToolStripMenuItem_Click);
+            // 
+            // コメントページを開くToolStripMenuItem
+            // 
+            this.コメントページを開くToolStripMenuItem.Name = "コメントページを開くToolStripMenuItem";
+            this.コメントページを開くToolStripMenuItem.Size = new System.Drawing.Size(166, 22);
+            this.コメントページを開くToolStripMenuItem.Text = "コメントページを開く";
+            this.コメントページを開くToolStripMenuItem.Click += new System.EventHandler(this.コメントページを開くToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(604, 591);
+            this.Controls.Add(this.radio_other);
+            this.Controls.Add(this.radio_kansya);
+            this.Controls.Add(this.radio_sub);
+            this.Controls.Add(this.radio_new);
+            this.Controls.Add(this.connect_button);
+            this.Controls.Add(this.userid_box);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "シンコメビュ";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem フォント設定ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+        private System.Windows.Forms.TextBox userid_box;
+        private System.Windows.Forms.Button connect_button;
+        private System.Windows.Forms.RadioButton radio_new;
+        private System.Windows.Forms.RadioButton radio_sub;
+        private System.Windows.Forms.RadioButton radio_kansya;
+        private System.Windows.Forms.RadioButton radio_other;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem コピーCToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ユーザーページを開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem コメントページを開くToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 最新までスクロールToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using Codeplex.Data;
+using System.Net.Http;
+using WebSocket4Net;
+
+namespace シンコメビュ
+{
+    public partial class Form1 : Form
+    {
+        private bool Is_connect = false;
+        private bool ScrollStop = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //バージョン更新時の処理
+            if (Properties.Settings.Default.IsUpgrade == false)
+            {
+                // Upgradeを実行する
+                Properties.Settings.Default.Upgrade();
+
+                // 「Upgradeを実行した」という情報を設定する
+                Properties.Settings.Default.IsUpgrade = true;
+
+                // 現行バージョンの設定を保存する
+                Properties.Settings.Default.Save();
+            }
+
+            //横幅・高さの設定
+            this.Width = Properties.Settings.Default.width;
+            this.Height = Properties.Settings.Default.height;
+
+
+            //ちらつき防止（コピペ
+            System.Type dgvtype = typeof(DataGridView);
+
+            // プロパティ設定の取得
+            System.Reflection.PropertyInfo dgvPropertyInfo =
+            dgvtype.GetProperty(
+            "DoubleBuffered", System.Reflection.BindingFlags.Instance |
+            System.Reflection.BindingFlags.NonPublic);
+
+            // 対象のDataGridViewにtrueをセットする
+            dgvPropertyInfo.SetValue(dataGridView1, true, null);
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnCount = 3;
+
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].Visible = false;
+
+
+            dataGridView1.Columns[0].Width = Properties.Settings.Default.width;
+            dataGridView1.Columns[0].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.AllowUserToAddRows = false;
+            //背景色変更
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.White;
+            //フォント変更
+            dataGridView1.RowsDefaultCellStyle.Font = Properties.Settings.Default.font;
+            //偶数行の背景色変更
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.WhiteSmoke;
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+            dataGridView1.Columns[0].ContextMenuStrip = this.contextMenuStrip1;
+        }
+        //useridの抽出
+        private string User_id()
+        {
+            if (userid_box.Text.Contains("twitcasting.tv"))
+            {
+                if (userid_box.Text.Contains("https://"))
+                {
+                    string rep = userid_box.Text.Replace("https://", "");
+                    string rep2 = rep.Replace("twitcasting.tv/", "");
+                    //Comment(rep2);
+                    //Console.WriteLine(rep2);
+                    return rep2;
+                }
+                else
+                {
+                    string rep = userid_box.Text.Replace("twitcasting.tv/", "");
+                    //Console.WriteLine(rep);
+                    return rep;
+                }
+            }
+            else
+            {
+                string streaming = userid_box.Text;
+                return streaming;
+
+            }
+
+        }
+
+        //WebRequestの短縮化
+        private string req(string url)
+        {
+            var wc = new WebClient();
+            var downloaddata = wc.DownloadData(url);
+            var enc = Encoding.UTF8.GetString(downloaddata);
+            return enc;
+        }
+
+        //接続ボタンが押されたときの処理
+        private void connect_button_Click(object sender, EventArgs e)
+        {
+            
+
+            if (!Is_connect && userid_box.Text != "")
+            {
+                Console.WriteLine(User_id());
+                //ユーザー存在チェックと配信中かチェック
+                string latest = req("https://frontendapi.twitcasting.tv/users/" + User_id() + "/latest-movie");
+                var js = DynamicJson.Parse(latest);
+                if (js.IsDefined("error"))
+                {
+                    MessageBox.Show("エラー：" + js.error.message + "(" + js.error.code + ")", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+                else
+                {
+                    if (js.movie.is_on_live)
+                    {
+                        userid_box.Enabled = false;
+                        connect_button.Text = "切断";
+                        radio_new.Enabled = false;
+                        radio_sub.Enabled = false;
+                        radio_kansya.Enabled = false;
+                        radio_other.Enabled = false;
+                        Is_connect = true;
+                        ws_connect();
+                    }
+                    else
+                    {
+                        MessageBox.Show("現在配信していません", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+            }
+            else
+            {
+                Is_connect = false;
+                userid_box.Enabled = true;
+                connect_button.Text = "接続";
+                radio_new.Enabled = true;
+                radio_sub.Enabled = true;
+                radio_kansya.Enabled = true;
+                radio_other.Enabled = true;
+            }
+        }
+
+        //wsの接続
+        private async void ws_connect()
+        {
+            string ws_ur = await ws_url();
+            Console.WriteLine("ws_url->OK");
+
+            Task tsk = Task.Run(() =>
+            {
+                var ws = new WebSocket(ws_ur);
+                ws.MessageReceived += Ws_MessageReceived;
+                ws.Open();
+
+                while (true)
+                {
+                    if (!Is_connect)
+                    {
+                        break;
+                    }
+                }
+                ws.Close();
+            });
+        }
+
+        //wsのメッセージを処理
+        private void Ws_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            string msg = e.Message;
+            if(msg != "[]")
+            {
+                var js = DynamicJson.Parse(msg);
+                for(int i = 0; js.IsDefined(i); i++)
+                {
+                    this.Invoke((MethodInvoker)(() => dataGridView1.Rows.Add(js[i].message, js[i].id, js[i].author.id)));
+                    if (!ScrollStop)
+                    {
+                        this.Invoke((MethodInvoker)(() => dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.Rows.Count - 1));
+                        this.Invoke((MethodInvoker)(() => ScrollStop = false));
+                        
+                    }
+                }
+            }
+        }
+
+
+        //movie_idの取得
+        private double movie_id()
+        {
+            string latest_movie = req("https://frontendapi.twitcasting.tv/users/" + User_id() + "/latest-movie");
+            var latest_json = DynamicJson.Parse(latest_movie);
+            return latest_json.movie.id;
+        }
+        
+        //ws_urlの取得(使いまわし)
+        private async Task<string> ws_url()
+        {
+            var bo = new Dictionary<string, string>()
+            {
+                {"movie_id", movie_id().ToString() }
+            };
+            var bo2 = new FormUrlEncodedContent(bo);
+            var hc = new HttpClient();
+            var tokenjs = await hc.PostAsync("https://twitcasting.tv/eventpubsuburl.php", bo2);
+            var token = DynamicJson.Parse(await tokenjs.Content.ReadAsStringAsync()).url;
+            return token;
+        }
+
+        //シンのradioが押された場合
+        private void radio_new_CheckedChanged(object sender, EventArgs e)
+        {
+            //textboxを触れなくする
+            userid_box.Enabled = false;
+            //textboxにidを入れる
+            userid_box.Text = "c:gomikuzunokiwami";
+        }
+
+        //サブのridioが押された場合
+        private void radio_sub_CheckedChanged(object sender, EventArgs e)
+        {
+            userid_box.Enabled = false;
+            userid_box.Text = "c:yurusunakimono";
+        }
+
+        //感謝のradioが押された場合
+        private void radio_kansya_CheckedChanged(object sender, EventArgs e)
+        {
+            userid_box.Enabled = false;
+            userid_box.Text = "c:kansyatyan";
+        }
+
+        //その他のradioが押された場合
+        private void radio_other_CheckedChanged(object sender, EventArgs e)
+        {
+            userid_box.Enabled = true;
+            userid_box.Text = "";
+        }
+
+        private void コメントページを開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitcasting.tv/" + User_id() + "/comment/" + movie_id().ToString() + "-" + dataGridView1.SelectedRows[0].Cells[1].Value.ToString());
+        }
+
+        private void コピーCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(dataGridView1.SelectedRows[0].Cells[0].Value.ToString());
+        }
+
+        private void ユーザーページを開くToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://twitcasting.tv/" + dataGridView1.SelectedRows[0].Cells[2].Value.ToString());
+        }
+
+        private void フォント設定ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog = new FontDialog();
+            fontDialog.Font = Properties.Settings.Default.font;
+            if(fontDialog.ShowDialog() != DialogResult.Cancel)
+            {
+                Properties.Settings.Default.font = fontDialog.Font;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        //右クリック時にセルを選択する
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                dataGridView1.ClearSelection();
+                dataGridView1.Rows[e.RowIndex].Selected = true;
+            }
+        }
+
+        //閉じるときにウィンドウのサイズを保存する
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.width = this.Width;
+            Properties.Settings.Default.height = this.Height;
+            Properties.Settings.Default.Save();
+        }
+
+        //スクロールしたときに自動スクロールを止める
+        private void dataGridView1_Scroll(object sender, ScrollEventArgs e)
+        {
+            ScrollStop = true;
+        }
+
+        private void 最新までスクロールToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.Rows.Count - 1;
+            ScrollStop = false;
+        }
+    }
+}
